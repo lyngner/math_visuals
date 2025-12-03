d576c692f474da40d68da356bbea0c50eb3dc422
lyngner <65701443+lyngner@users.noreply.github.com>
2025-12-03 21:11:13 +0100

    Update axis arrow SVGs to outlined style

--- graftegner.js
+++ graftegner.js
@@ -230,9 +230,9 @@ const AXIS_LABEL_MARGIN_FRACTION = 0.005;
 const AXIS_LABEL_MARGINS = { x: AXIS_LABEL_MARGIN_FRACTION, y: AXIS_LABEL_MARGIN_FRACTION };
 
 const X_AXIS_ARROW_SVG_TEMPLATE =
-  '<svg width="17" height="30" viewBox="0 0 17 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.1421 16.1421C16.9231 15.3611 16.9231 14.0948 16.1421 13.3137L3.41417 0.5858C2.63313 -0.195248 1.3668 -0.195248 0.585748 0.5858C-0.195301 1.36685 -0.195301 2.63318 0.585748 3.41423L11.8995 14.7279L0.585748 26.0416C-0.195301 26.8227 -0.195301 28.089 0.585748 28.8701C1.3668 29.6511 2.63313 29.6511 3.41417 28.8701L16.1421 16.1421ZM14.7278 14.7279V16.7279H14.7279V14.7279V12.7279H14.7278V14.7279Z" fill="{{COLOR}}"/></svg>';
+  '<svg width="17" height="30" viewBox="0 0 17 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1 L15 15 L1 29" stroke="{{COLOR}}" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none"/></svg>';
 const Y_AXIS_ARROW_SVG_TEMPLATE =
-  '<svg width="30" height="17" viewBox="0 0 30 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.1422 0.585778C15.3612 -0.195271 14.0949 -0.195271 13.3138 0.585778L0.585892 13.3137C-0.195157 14.0947 -0.195157 15.3611 0.585892 16.1421C1.36694 16.9232 2.63327 16.9232 3.41432 16.1421L14.728 4.82842L26.0417 16.1421C26.8228 16.9232 28.0891 16.9232 28.8702 16.1421C29.6512 15.3611 29.6512 14.0947 28.8702 13.3137L16.1422 0.585778ZM14.728 2.00009L16.728 2.00009V1.99999L14.728 1.99999L12.728 1.99999V2.00009L14.728 2.00009Z" fill="{{COLOR}}"/></svg>';
+  '<svg width="30" height="17" viewBox="0 0 30 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 16 L15 2 L29 16" stroke="{{COLOR}}" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none"/></svg>';
 
 function axisArrowSvgData(axis, color) {
   const template = axis === 'y' ? Y_AXIS_ARROW_SVG_TEMPLATE : X_AXIS_ARROW_SVG_TEMPLATE;
