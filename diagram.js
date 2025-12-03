74ed94f286b50fe15ec16b34abd380c42f27ddf2
lyngner <65701443+lyngner@users.noreply.github.com>
2025-12-03 21:48:39 +0100

    Update axis arrow SVG templates

--- diagram.js
+++ diagram.js
@@ -107,11 +107,11 @@ const LEGACY_LEGEND_BACKGROUND = '#ffffff';
 const LEGACY_LEGEND_TEXT_COLOR = '#000000';
 
 const X_AXIS_ARROW_SVG_TEMPLATE =
-  '<svg width="30" height="17" viewBox="0 0 30 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1 L29 8.5 L1 16" stroke="{{COLOR}}" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none"/></svg>';
+  '<svg width="17" height="30" viewBox="0 0 17 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1 L15 15 L1 29" stroke="{{COLOR}}" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none"/></svg>';
 const Y_AXIS_ARROW_SVG_TEMPLATE =
-  '<svg width="17" height="30" viewBox="0 0 17 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 29 L8.5 1 L16 29" stroke="{{COLOR}}" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none"/></svg>';
-const X_AXIS_ARROW_SIZE = { width: 30, height: 17 };
-const Y_AXIS_ARROW_SIZE = { width: 17, height: 30 };
+  '<svg width="30" height="17" viewBox="0 0 30 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 16 L15 2 L29 16" stroke="{{COLOR}}" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none"/></svg>';
+const X_AXIS_ARROW_SIZE = { width: 17, height: 30 };
+const Y_AXIS_ARROW_SIZE = { width: 30, height: 17 };
 
 function getAxisColor() {
   if (!svg || typeof getComputedStyle !== 'function') {
